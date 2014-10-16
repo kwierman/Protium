250c1a2a91143d64b1b86e1c1702a0fb5fcc40c9
Kevin Wierman <kwierman@gmail.com>
2014-10-16 08:21:08 -0400

    Adding in some documentation changes

---
+++ Design/Assert.hh
@@ -0,0 +1,39 @@
+#ifndef Protium_Assert_hh_
+#define Protium_Assert_hh_
+
+#include <stdlib.h>
+#include <iostream>
+
+/** \file Assert.hh
+	\author Kevin Wierman
+	\date April 12th, 2014
+	\brief Defines static and runtime assertions.
+
+	These are to be used as follows:
+
+	~~~~~~~~
+        PROTIUM_RUNTIME_ASSERT(true, ThisIsATrueStatement);
+        PROTIUM_RUNTIME_ASSERT(false, ThisIsAFalseStatement);
+	~~~~~~~~
+
+	\note The second argument must be a single word, or else it will be interpreted as several arguements.
+**/
+
+
+//! Body-less template
+template<int> 
+struct CompileTimeError;
+
+//! Fully-defined template
+template<> 
+struct CompileTimeError<true> {};
+
+//! Use for performing static assertion
+#define PROTIUM_STATIC_ASSERT(expr, msg) \
+    { CompileTimeError<((expr) != 0)> ERROR_##msg; (void)ERROR_##msg; } 
+    
+//! Use for runtime assertions
+#define PROTIUM_RUNTIME_ASSERT(expr, msg)\
+    {if(!expr) std::cerr<<##msg<<std::endl; abort();}
+
+#endif //Protium_Assert_h_
---
+++ Design/Constructable.hh
@@ -0,0 +1,92 @@
+#ifndef Protium_Constructable_h_
+#define Protium_Constructable_h_
+
+#include <map>
+#include <string>
+
+/**
+	\file Constructable.hh
+	\author Kevin Wierman
+	\date September 11, 2013
+
+	\brief Objects for constructing instances from strings
+
+	To be used as follows:
+
+	In class header:
+	
+	~~~~~~~~
+	class MyClass : ConstructablePrototype{
+	    REGISTER_DECLARE_TYPE(MyClass, MyGroup);
+	}
+	~~~~~~~~
+
+	In class body:
+
+	~~~~~~~~
+	REGISTER_DEF_TYPE(MyNamespace::MyClass, MyGroup);
+	~~~~~~~~
+
+	Then, when you want to construct an instance:
+
+	~~~~~~~~
+	MyClass* instance = dynamic_cast<MyClass>(VirtualFactory<MyGroup>.createInstance("MyClass") );
+	~~~~~~~~
+
+**/
+
+namespace Protium{
+	namespace Design{
+
+		//! Prototype class to be used as a base class for constructible classes
+		struct ConstructablePrototype{
+			ConstructablePrototype(){}
+			virtual ~ConstructablePrototype(){}
+		};
+
+		//! A factory for constructing instances from strings
+		template<class Group>
+		struct VirtualFactory{
+			typedef std::map< std::string, ConstructablePrototype*(*)() > map_type;
+		
+			//! Returns instance of constructible class for a string s
+			static ConstructablePrototype* createInstance(const std::string& s, ConstructablePrototype*(*creator)()=0 ){
+				//! \todo: See if we can remove the creator field
+				map_type::iterator it = getMap()->find(s);
+				if( it==getMap()->end() ){
+					return NULL;
+				}
+				return it->second();
+			}
+		protected:
+			//! Accesses the map field for all classes of group, Group.
+			static map_type* getMap(){
+				static map_type* map = new map_type();
+				return map;
+			}
+		};
+
+		//! Register used to insert mappings from string to class constructor
+		template<class T, class Group>
+		struct DerivedRegister : VirtualFactory<Group>{
+			//! Function signature for creating instances of classes
+			static ConstructablePrototype* create() {return (new T()); }
+
+			//! Definition of insertion of class into map
+			DerivedRegister(const std::string& s ){
+				getMap->insert(std::make_pair(s, &create) );
+			}
+
+		};
+	}
+}
+
+//! Declares a register for a given constructible class
+#define REGISTER_DEC_TYPE(NAME, GROUP) \
+    static Protium::Design::DerivedRegister<NAME, GROUP> reg;
+
+//! Defines a register for a given constructible class
+#define REGISTER_DEF_TYPE(NAME, GROUP) \
+    Protium::Design::DerivedRegister<NAME, GROUP> NAME::reg(#NAME);
+
+#endif //File Guardian
