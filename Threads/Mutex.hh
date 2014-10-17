dd201dc7edc89a9be87c9ba225e3f99a8bf39768
Kevin Wierman <kwierman@gmail.com>
2014-10-17 06:17:03 -0400

    Redoing a whole bunch of things, and not deleting threading policies, which may be the most useful thing in this module

---
+++ Threads/Mutex.hh
@@ -0,0 +1,52 @@
+/*! \file Mutex.h
+    \brief Defines a very basic object-oriented mutex.
+    \author Kevin Wierman
+ */
+
+#ifndef Protium_Threads_hh_
+#define Protium_Threads_hh_
+
+#include <pthread.h> //For access to pthreads
+
+namespace Protium{
+    namespace Threads{
+
+        /*! \class Mutex
+            \brief Wrapper class for pthreads-style mutexes
+        
+            Provides the ability to lock mutexes at the object level.
+            Use as such:
+
+            ~~~~~~~~
+                Mutex mtx;
+                mtx.Lock();
+                if(mtx.IsLocked() )
+                {
+                    ...do some things...
+                }
+                mtx.Unlock();
+            ~~~~~~~~
+
+         */
+        class Mutex{
+            Mutex(const Mutex&);
+            Mutex & operator=(const Mutex& );
+            pthread_mutex_t fMtx;
+            bool fIsLocked;
+        public:
+            Mutex();
+            //! Attempts to lock the mutex and blocks until mutex is available. 
+            void Lock();
+            //! Unlocks the mutex for the next usage.
+            void Unlock();
+            /*! Checks to see if the  mutex if locked
+            *   \return True if the mutex is locked elsewhere. False otherwise.
+            */
+            bool IsLocked();
+        };
+    }
+}
+
+
+
+#endif //Protium_Threads_h_
---
+++ Threads/Threads.hh
@@ -0,0 +1,14 @@
+#ifndef Protium_Threads_hh_
+#define Protium_Threads_hh_
+
+#include "Protium/Threads/Mutex.hh"
+#include "Protium/Threads/ThreadingPolicy.hh"
+
+namespace Protium{
+
+	//! Encapsulates threading policies and objects
+	namespace Threads{}
+}
+
+
+#endif //Protium_Threads_h_
