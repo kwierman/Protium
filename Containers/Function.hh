b7a180366d29fce078b6e7969144c7404fc7282d
Kevin Wierman <kwierman@gmail.com>
2014-12-16 13:45:02 -0500

    Further Cleanup

---
+++ Containers/Function.hh
@@ -0,0 +1,20 @@
+#ifndef Protium_Function_hh
+#define Protium_Function_hh
+
+namespce Protium{
+
+	namespace Containers{
+
+		template<typename XType, typename YType>
+		class Function : public Functional1D<XType, YType>{
+			/*!
+				//! 
+			*/
+
+
+		};
+	}
+}
+
+
+#endif //File Guardian
---
+++ Utilities/Utilities.hh
@@ -0,0 +1,13 @@
+#ifndef Protium_Utilities_Utilities_hh
+#define Protium_Utilities_Utilities_hh
+
+#include "Protium/Utilities/Endian.hh"
+#include "Protium/Utilities/SignalCatching.hh"
+
+namespace Protium{
+
+	//! Holds miscellanious utilites
+	namespace Utilities{}
+}
+
+#endif //File Guardian
