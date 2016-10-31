9799dd379db59e2a9d7eeac40771f84f988f97c0
Kevin Wierman <kwierman@gmail.com>
2016-10-31 10:38:44 -0700

    Update Thread.cc
    Replacing braces with spaces in places where braces cause stasis.

--- Threads/Thread.cc
+++ Threads/Thread.cc
@@ -1,12 +1,12 @@
 #include "Protium/Threads/Thread.hh"
 namespace Protium{
-	namespace Threads{
-		namespace Private{
-			void* threadingFunction(void* thread){
-				Thread* t = static_cast<Thread*>(thread);
-				t->start();
-				return NULL;
-			}
-		}
-	}
+    namespace Threads{
+        namespace Private{
+            void* threadingFunction(void* thread){
+                Thread* t = static_cast<Thread*>(thread);
+                t->start();
+                return NULL;
+            }
+        }
+    }
 }
